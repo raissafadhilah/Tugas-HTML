191a6b3e3be6c5010e189e794953f9f3b9c6db5f
raissafadhilah <118705915+raissafadhilah@users.noreply.github.com>
2023-08-07 15:17:47 +0700

    Tugas PWB

---
+++ raisa.dart
@@ -0,0 +1,28 @@
+void main() {
+    print('assalamualaikum');
+
+// numerik
+var a = 100;
+var b = 200;
+var hasil = a + b;
+print('$hasil');
+
+//non numerik
+var kata1 = "selamat datang";
+var kata2 = " upin & ipin";
+var kata3 = " di kondangan kak ros";
+print(kata1 + kata2 + kata3);
+
+///coba coba
+  
+var hari1 = "selamat pagi";
+var hari2 = " selamat menjalani hari ini";
+var hari3 = " semoga bahagia selalu";
+print(hari1 + hari2 + hari3);
+
+//coba
+var nama = "nama saya : raissa fadhillah";
+var kelas = " kelas : xi rpl 2";
+var sekolah = " asal sekolah : smkn 1 karawang";
+print(nama + kelas + sekolah);
+}
---
+++ sa.js
@@ -0,0 +1,8 @@
+var evenNumber = []
+
+for(let i =2; i <= 100; i++){
+    if(i%2==0){
+        evenNumber.push(i)
+    }
+}
+console.log(evenNumber)  
